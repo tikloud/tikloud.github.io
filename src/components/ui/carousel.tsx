009b21045fccf1cd986d592fc55d8686200d86d3
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2026-06-17 21:32:28 +0900

    Feat: Add Supabase

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -95,12 +95,18 @@ function Carousel({
 
   React.useEffect(() => {
     if (!api) return
-    onSelect(api)
-    api.on("reInit", onSelect)
-    api.on("select", onSelect)
+
+    const handleSelect = () => onSelect(api)
+    
+    api.on("reInit", handleSelect)
+    api.on("select", handleSelect)
+    
+    // Initial update
+    handleSelect()
 
     return () => {
-      api?.off("select", onSelect)
+      api?.off("select", handleSelect)
+      api?.off("reInit", handleSelect)
     }
   }, [api, onSelect])
 
